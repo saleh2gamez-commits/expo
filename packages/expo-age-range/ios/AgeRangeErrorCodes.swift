3d91042782f9cc79fe1ea1479c43f7f83c9ee99d
Vojtech Novak <vonovak@gmail.com>
2025-10-20 09:37:16 +0200

    [age-range] implement ios module for DeclaredAgeRange apis

---
+++ packages/expo-age-range/ios/AgeRangeErrorCodes.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+internal struct AgeRangeErrorCodes {
+  static let featureUnsupported = "ERR_AGE_RANGE_FEATURE_UNSUPPORTED"
+  static let notAvailable = "ERR_AGE_RANGE_NOT_AVAILABLE"
+  static let invalidRequest = "ERR_AGE_RANGE_INVALID_REQUEST"
+  static let userDeclined = "ERR_AGE_RANGE_USER_DECLINED"
+}
